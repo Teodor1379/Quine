7299412a2254c73dad8ab14ec8d79db4c016ec80
Teodor1379 <teodor.s.dishanski.2003.1379@gmail.com>
2024-09-24 14:07:48 +0300

    Commit: Main

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,18 @@
+#include <fstream>
+#include <iostream>
+
 int main() {
+    std::ifstream stream(__FILE__);
+
+    char c = '\0';
+
+    while (stream.good()) {
+        stream.get(c);
+
+        std::cout.put(c);
+    }
+
+    stream.close();
+
     return 0;
 }
